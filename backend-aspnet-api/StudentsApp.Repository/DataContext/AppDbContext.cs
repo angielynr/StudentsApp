cc011a106f691a3cb7765bea882ba37099560cdf
Angielyn Rivera <2194059@cognizant.com>
2023-07-06 16:33:48 +0800

    initial commit

---
+++ backend-aspnet-api/StudentsApp.Repository/DataContext/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsApp.Repository.Entities;
+
+namespace StudentsApp.Repository.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        //DBTable
+        public DbSet<Student> students { get; set; }
+
+    }
+}
---
+++ backend-aspnet-api/StudentsApp.Repository/Migrations/20230706065507_AddStudentTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentsApp.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "students");
+        }
+    }
+}
