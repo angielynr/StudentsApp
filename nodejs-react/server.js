82d0907d489231a48b143d503cfcd17f7323f57b
Angielyn Rivera <2194059@cognizant.com>
2023-07-10 14:18:04 +0800

    Setup database connection nodejs - mongoDb

--- nodejs-react/server.js
+++ nodejs-react/server.js
@@ -1,12 +1,39 @@
 const express = require("express");
-const PORT = 5002;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
+const port = {
+    http: 5003,
+    https: 5004,
+};
+
+app.use(bodyParser.json());
+
+const router = express.Router();
+
+// router.route("/api/students").get((req, res) => {
+//     console.log("test get method");
+//     res.status(200).json({
+//         data: "test get",
+//         message: "get method success",
+//     });
+// });
 
 app.get("/", (req, res) => {
-  res.send("Hello Worlds");
+    res.status(200).json({ message: "Hello from my-express-app!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
-});
+// database connection
+mongoose
+    .connect("mongodb://127.0.0.1:27017/studentDb")
+    .then(() => {
+        console.log("Connected to MongoDb");
+        app.listen(port.http, (err) => {
+            console.log(`Server started at http://localhost:${port.http}`);
+            if (err) console.log("Error" + err);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
