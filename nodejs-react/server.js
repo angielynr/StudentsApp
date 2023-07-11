920420fad399fbf774e8a69bb1e879a5e4532b47
Angielyn Rivera <2194059@cognizant.com>
2023-07-11 18:59:26 +0800

    Refactor code, added controller for student

--- nodejs-react/server.js
+++ nodejs-react/server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const Student = require("./models/student");
 const bodyParser = require("body-parser");
+const studentController = require("./controllers/studentsController");
 
 const app = express();
 const router = express.Router();
@@ -10,83 +10,18 @@ const router = express.Router();
 app.use(bodyParser.json());
 app.use(router);
 
-router.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello from StudentApp!" });
-});
+router.route("/").get(studentController.home);
 
-router.get("/api/students", async (req, res) => {
-    try {
-        const student = await Student.find(res.body);
-        res.status(200).json(student);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({
-            message: error.message,
-        });
-    }
-});
+router
+    .route("/api/students")
+    .get(studentController.getAllStudents)
+    .post(studentController.createStudent);
 
-router.get("/api/students/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const student = await Student.findById(id);
-        res.status(200).json(student);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({
-            message: error.message,
-        });
-    }
-});
-
-router.post("/api/students", async (req, res) => {
-    try {
-        const student = await Student.create(req.body);
-        res.status(200).json(student);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({
-            message: error.message,
-        });
-    }
-});
-
-router.put("/api/students/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const student = await Student.findByIdAndUpdate(id, req.body);
-        if (!student) {
-            return res.status(404).json({
-                message: `cannot find student with ID: ${id}`,
-            });
-        }
-        const updatedStudent = await Student.findById(id);
-        res.status(200).json(updatedStudent);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({
-            message: error.message,
-        });
-    }
-});
-
-router.delete("/api/students/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const student = await Student.findByIdAndDelete(id);
-        if (!student) {
-            return res.status(404).json({
-                message: `cannot find student with ID: ${id}`,
-            });
-        }
-        res.status(200).json(student);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({
-            message: error.message,
-        });
-    }
-});
+router
+    .route("/api/students/:id")
+    .get(studentController.getStudent)
+    .put(studentController.updateStudent)
+    .delete(studentController.deleteStudent);
 
 // database connection
 mongoose
